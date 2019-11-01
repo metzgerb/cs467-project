b196bc3a95f3730006366d9e8943612ae5f8c0d2
beallch <38113042+beallch@users.noreply.github.com>
2019-11-01 14:10:07 -0500

    FIxed so that server works locally

--- DataTransfer/testServer.js
+++ DataTransfer/testServer.js
@@ -7,7 +7,7 @@
 
 var port = process.env.PORT || 8080
 
-const dataTransfer = require('DataTransfer/dataTransferFunctions');
+const dataTransfer = require('./dataTransferFunctions');
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
