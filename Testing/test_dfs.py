d44e9996ed9bcfc944260e2cd4de28de77dca174
metzgerb <40550164+metzgerb@users.noreply.github.com>
2019-11-24 13:01:39 -0600

    fix tests for robots change

--- Testing/test_dfs.py
+++ Testing/test_dfs.py
@@ -44,9 +44,7 @@ for x in range(10):
     if DEBUG:
         print("Testing DFS with depth of 3: " + ROOT_URL + "root.html")
     
-    robots = cu.get_robots(ROOT_URL + "root.html")
-    
-    test_tree = cu.depth_search(ROOT_URL + "root.html", 3, robots)
+    test_tree = cu.depth_search(ROOT_URL + "root.html", 3)
     tree_list = []
 
     for link in test_tree:
@@ -100,10 +98,8 @@ Expected Output: Tree containing 2 or 3 links
 for x in range(10):
     if DEBUG:
         print("Testing DFS with depth of 3: " + ROOT_URL + "root.html")
-
-    robots = cu.get_robots(ROOT_URL + "root.html")
     
-    test_tree = cu.depth_search(ROOT_URL + "root.html", 3, robots, "monkey")
+    test_tree = cu.depth_search(ROOT_URL + "root.html", 3, "monkey")
     tree_list = []
 
     for link in test_tree:
@@ -149,10 +145,8 @@ Expected Output: Tree containing 0 links
 """
 if DEBUG:
     print("Testing DFS with depth of 3: https://www.google.com/search")
-
-robots = cu.get_robots("https://www.google.com/search")
     
-test_tree = cu.depth_search("https://www.google.com/search", 3, robots)
+test_tree = cu.depth_search("https://www.google.com/search", 3)
 tree_list = []
 
 for link in test_tree:
@@ -188,9 +182,7 @@ Expected Output: Tree containing more than 0 links
 if DEBUG:
     print("Testing DFS with depth of 3: https://www.google.com/finance")
 
-robots = cu.get_robots("https://www.google.com/finance")
-    
-test_tree = cu.depth_search("https://www.google.com/finance", 3, robots)
+test_tree = cu.depth_search("https://www.google.com/finance", 3)
 tree_list = []
 
 for link in test_tree:
